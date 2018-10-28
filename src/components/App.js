5ac8ef9299711c53a402385f00085b4c7ec69c83
MatthewDodi <matthew.dodi@gmail.com>
2018-10-28 14:45:25 -0700

    type: feat
    * passed down all data needed to render individual tweets in to the Tweet.js component
    * implemented a check to see if the tweet.id exist and if it doesnt an error will be displayed on screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
+import Dashboard from './Dashboard';
 
 class App extends Component {
   componentDidMount() {
@@ -10,9 +11,18 @@ class App extends Component {
   render() {
     return (
       <div>
-        
+        {this.props.loading === true
+        ? null
+        : <Dashboard />
+        }
       </div>
     )
+  }
+}
+
+const mapStateToProps = ({ authedUser }) => {
+  return {
+    loading: authedUser === null
   }
 }
 
