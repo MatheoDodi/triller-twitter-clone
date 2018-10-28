dd05788474aea69608f418820a30e071704aefe4
MatthewDodi <matthew.dodi@gmail.com>
2018-10-28 13:37:43 -0700

    type: feat
    * Added redux-thunk and applied middleware

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import combineReducers from './reducers';
+import middleware from './middleware';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+store = createStore(combineReducers, middleware);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'))
