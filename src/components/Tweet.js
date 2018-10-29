85c032ea1e1a502ecc1f6373b7cda5bda16f7ebf
MatthewDodi <matthew.dodi@gmail.com>
2018-10-28 18:10:49 -0700

    type: feat
    * added react-redux-loading package to show a loding bar while the app fetches the initialData
    * completed the Tweet.js component

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,16 +1,54 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { formatTweet } from '../utils/helpers';
+import { formatTweet, formatDate } from '../utils/helpers';
+import { TiArrowBackOutline, TiHeartFullOutline, TiHeartOutline } from 'react-icons/ti';
+
 
 class Tweet extends Component {
+  toParent = (e, parentId) => {
+    e.preventDefault();
+  }
+
+  handleLike = (e) => {
+    e.preventDefault
+  }
 
   render() {
     const { tweet } = this.props;
     if ( tweet === null ) return <p>This Trill doesn't exist</p>
 
+    const {
+      name,
+      avatar,
+      timestamp,
+      text,
+      hasLiked,
+      likes,
+      replies,
+      parent
+    } = tweet;
+
     return (
       <div className='tweet'>
-        Hi
+        <img src={avatar} alt={`avatar of ${name}`} className='avatar' />
+        <div className='tweet-info'>
+          <div>
+            <span>{name}</span>
+            <div>{formatDate(timestamp)}</div>
+            {parent && (
+              <button className='replying-to' onClick={(e) => this.toParent(e, parent.id)}>
+                Replying to @{parent.author}
+              </button>
+            )}
+            <p>{text}</p>
+          </div>
+          <div className='tweet-icons'>
+            <TiArrowBackOutline className='tweet-icon' />
+            <span>{replies !== 0 && replies}</span>
+            <button className='heart-button' onClick={this.handleLike}>{hasLiked ? <TiHeartFullOutline color='#E0245E' className='tweet-icon' /> : <TiHeartOutline className='tweet-icon' />}</button>
+            <span>{likes !==0 && likes}</span>
+          </div>
+        </div>
       </div>
     )
   }
